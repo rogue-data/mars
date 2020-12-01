8d2375ee1393a0030408e39995bfd2cd2537efbb
Sagar-Kumar-007 <sk8814594@gmail.com>
2020-12-01 15:42:47 +0530

    Django App created, Working Template Created

---
+++ src/assets/Working Directory/song_url.py
@@ -0,0 +1,15 @@
+from fetch_data_api import sp_api
+
+
+def song_url(name):
+    client_id = 'e55a0893bd7f4c7e9628fe6ee58f4324'
+    client_secret = 'a434335709d84dac9c222a941db65c30'
+    sp = sp_api(client_id,client_secret)
+    result = sp.search(q=name, limit=1)
+    result = result['tracks']
+    result = result['items']
+    result = result[0]
+    return (result['external_urls'])['spotify']
+
+
+#print(song_url('tum hi ho'))

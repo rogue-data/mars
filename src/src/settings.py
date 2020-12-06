95d956a2aa8cf49277138eb464a58d198c1ea35d
coastaldemigod <shivanshagrawal2411@gmail.com>
2020-12-06 13:46:56 +0530

    files optimized for deployment

--- src/src/settings.py
+++ src/src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('./src/etc/secret_key.txt') as f:
+with open('./src/etc/secret_key.txt') as f: # when hosting on pythonanywhere , change this url to absolute
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!

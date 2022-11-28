5f7a015ada7b5723379a8c2afac4bd8255f1c024
Sagar-Kumar-007 <sk8814594@gmail.com>
2022-11-29 00:35:59 +0530

    Pafy alternative found

--- src/mars_app/wd/download_songs.py
+++ src/mars_app/wd/download_songs.py
@@ -1,24 +1,12 @@
 from youtube_search import YoutubeSearch
-import pafy
+from subprocess import Popen,PIPE
+import subprocess,sys
 # import os
   
 def download_song(name,album):
-    # path=os.path.join(os.path.dirname(os.path.dirname(__file__)),'static/songs/')
-    # os.chdir(path)
     print('d start')
-    # try:
-    #     os.remove('0.mp3')
-    # except:
-    #     print("nothing")
     result = YoutubeSearch(name+''+album, max_results=1).to_dict()
     url='https://www.youtube.com/watch?v='+result[0]['id']
-    video = pafy.new(url) 
-    audiostreams = video.audiostreams 
-    # audiostreams[0].download()
-    #renaming
-    #print(f"Location: {os.listdir()}")
-    # os.rename((os.listdir())[0],"0.mp3")
+    x=str(subprocess.check_output(["youtube-dl","-g","-x", url])).split("\\n")[0].split('b\'')[1]
     print('d end')
-    # print(audiostreams[0].url)
-    return audiostreams[0].url
-# download_song('Darkside')
+    return x
